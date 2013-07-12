0a7fc00a3f04de2147131b098c36de1acfcf4412
Robert de Vries <r.devries@robbytu.net>
2013-07-12 02:25:52 +0200

    Finished implementation

--- src/main/java/net/robbytu/banjoserver/servers/Main.java
+++ src/main/java/net/robbytu/banjoserver/servers/Main.java
@@ -1,6 +1,8 @@
 package net.robbytu.banjoserver.servers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -9,15 +11,11 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         this.plugin = this;
-
-        getCommand("servers").setExecutor(new CommandHandler());
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-
-
     }
 
     @Override
-    public void onDisable() {
-
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return (new CommandHandler().onCommand(sender, cmd, label, args));
     }
 }
